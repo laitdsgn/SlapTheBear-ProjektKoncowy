2f250a36a9942c06ba6c90996c6386cff6ddb3ed
laitdsgn <tomek08.konto@gmail.com>
2023-03-17 20:54:39 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -12,20 +12,14 @@ startbutton.addEventListener('click', function() {
     document.getElementById("start_button").style.visibility = "hidden"
 
 
-    // losowanie dziury z której wyskoczy miś
-//    const i = Math.floor(Math.random() * (dziury.length));
-//    const losowadziura = dziury[i];
-
-//TODO: wolne wyskakiwanie miskow po czasie (pewnie spowodowane tym że dane misie mają już class odpowiadającą za wyskakiwanie - w skrócie trzeba usunąć class po schowaniu sie misia)
-
 setInterval(() => {
 
+    // dodawanie animacji wyskakiwania 
     const x = Math.floor(Math.random() * (dziury.length));
     const losowymis = misie[x]
- 
     losowymis.classList.add('wyskakiwanie')
 
-    
+    // usuwanie animacji wyskakiwania
     setTimeout(() => {
         losowymis.classList.remove('wyskakiwanie'); 
     }, 750);
