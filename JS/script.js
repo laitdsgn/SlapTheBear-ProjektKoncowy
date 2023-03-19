69246b40590169ecc441ad70e5214d99459153ec
laitdsgn <tomek08.konto@gmail.com>
2023-03-19 20:21:53 +0100

    points can decrease now

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,7 @@ let dziury = document.querySelectorAll('.hole')
 let misie = document.querySelectorAll('.bear')
 let punkty = document.getElementById('points')
 let punktywartosc = 0
+let punktywartoscminus = 0
 
 const startbutton = document.getElementById('start_button')
 
@@ -31,43 +32,17 @@ setInterval(() => {
 }, 900);
 
 
-
     misie.forEach(mis => mis.addEventListener('click', function() {
-        punktywartosc +=  10;
-        punkty.innerText = punktywartosc;
+        punktywartosc += 20 // 20 ponieważ naciskając na misia naciskamy jednoczesnie na dziurę więc -10 + 20 = 10
+        punkty.innerText = punktywartosc 
+        console.log(punkty.innerText);
     }));
 
+    dziury.forEach(dziura => dziura.addEventListener('click', function() {
+        punktywartoscminus += -10
+        punkty.innerText = punktywartoscminus + punktywartosc
+    }));
 
 });
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
